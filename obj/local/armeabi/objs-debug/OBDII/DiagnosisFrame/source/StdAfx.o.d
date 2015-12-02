9b0f624d45ac7139fb410fc4df56fb0e0dc12106
andy <1695981713@qq.com>
2015-12-02 21:21:52 +0800

    delete debug information

--- obj/local/armeabi/objs-debug/OBDII/DiagnosisFrame/source/StdAfx.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs-debug/OBDII/DiagnosisFrame/source/StdAfx.o: \
- jni/DiagnosisFrame/source/StdAfx.cpp jni/DiagnosisFrame/include/stdafx.h
-
-jni/DiagnosisFrame/include/stdafx.h:
